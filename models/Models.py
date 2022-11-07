79fc537c1feb8b37899a193aecdff9d2b756096f
harriee02 <taiwooriowo022gmail.com>
2022-11-06 22:56:04 -0500

    user's prompt removed

--- models/Models.py
+++ models/Models.py
@@ -5,8 +5,6 @@ url: str = 'https://xnxjedytmfutvwvycojq.supabase.co'
 key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhueGplZHl0bWZ1dHZ3dnljb2pxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njc2OTY2OTgsImV4cCI6MTk4MzI3MjY5OH0.YYKFIfM8DHWdbW4HI_iS1ysCL0bsvKAN75dDwQNpnCk"
 supabase: Client = create_client(url, key)
 
-username = input("Email: ")
-password = input("Password: ")
 loginDetails = None
 try:
     loginDetails = supabase.auth.sign_in(email=username, password=password)
@@ -15,8 +13,6 @@ except:
     print("You can try again! Or create a new user by typing 'create an account'")
     
 def signUp():
-    username = input("Email: ")
-    password = input("Password: ")
     loginDetails = supabase.auth.sign_up(email=username, password=password)
     
 
@@ -63,4 +59,19 @@ def deleteSong(songName, playListName):
                     supabase.table("playlist").update({"songs":i["songs"]}).eq("playlistname",playListName).eq("createdby", str(loginDetails.user.id)).execute()
                     return "Song has been successfully deleted!!"
     return "\n" + playListName + "'s playlist does not exist!"
+
+def viewPlaylists():
+    playlistList = supabase.table("playlist").select("playlistname").eq("createdby", str(loginDetails.user.id)).execute()
+    for i in playlistList.data:
+        print(i["playlistname"])
+    return
+
+def viewSongsPerPlaylist(playListName):
+    songsList = supabase.table("playlist").select("songs").eq("createdby", str(loginDetails.user.id)).eq("playlistname", playListName).execute()
+    for i in songsList.data:
+        for j in i["songs"]:
+            print(j)
+        break
+    return 
+
 
